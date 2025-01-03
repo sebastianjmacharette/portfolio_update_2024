93d73037be96e642816d8de7cf089aa8deb9c86e
sebastianjmacharette <dev@macharette.com.ar>
2025-01-03 18:10:18 -0300

    update 03012025

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../../assets/img/hero.webp';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function Home() {
   const navigate = useNavigate();
@@ -11,39 +12,58 @@ function Home() {
 
   return (
     <div
-      className="relative min-h-screen bg-cover bg-center"
+      className="relative min-h-screen h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${Hero})` }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-70"></div> {/* Capa de opacidad */}
       <div className="relative z-10 flex items-center justify-center h-full">
-        <div className="hero-content flex flex-col lg:flex-row p-10 rounded-lg shadow-lg">
-        <div className="relative w-full max-w-[90%] sm:max-w-[70%] md:max-w-[50%] lg:max-w-[35%] pb-[112.5%] sm:pb-[44.45%] rounded-lg shadow-2xl mx-auto">
-  <iframe
-    className="absolute top-0 left-0 w-full h-full rounded-lg"
-    src="https://www.youtube.com/embed/bL58jnbN7yE"
-    title="YouTube video player"
-    frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-  ></iframe>
-</div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="hero-content flex flex-col lg:flex-row p-10 rounded-lg shadow-lg"
+        >
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+            className="relative w-full max-w-[90%] sm:max-w-[70%] md:max-w-[50%] lg:max-w-[35%] pb-[112.5%] sm:pb-[44.45%] rounded-lg shadow-2xl mx-auto"
+          >
+            <iframe
+              className="absolute top-0 left-0 w-full h-full rounded-lg"
+              src="https://www.youtube.com/embed/bL58jnbN7yE"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </motion.div>
 
-
-          <div className="lg:ml-10 mt-6 lg:mt-0 flex   flex-col lg:items-end w-full">
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            className="lg:ml-10 mt-6 lg:mt-0 flex flex-col lg:items-end w-full"
+          >
             <h1 className="text-5xl font-bold sm:text-center text-white lg:text-end">Portfolio Digital</h1>
-            <h2 className="text-2xl font-bold   text-white ">Programador Full Stack</h2>
-            <h2 className="text-2xl font-bold   text-white ">Especializado en Frontend</h2>
-            <h2 className="text-2xl font-bold   text-white ">Diseñador UX/UI</h2>
+            <h2 className="text-2xl font-bold text-white">Programador Full Stack</h2>
+            <h2 className="text-2xl font-bold text-white">Especializado en Frontend</h2>
+            <h2 className="text-2xl font-bold text-white">Diseñador UX/UI</h2>
 
             <p className="p-10 font-semibold text-xl hidden lg:block text-white text-end">
-              "Portfolio Digital de <span className='bg-secondary'>Sebastián Macharette</span> . Gracias por conocer más
-               sobre mí, mis <span className='bg-secondary'>estudios y proyectos</span>."
+              "Portfolio Digital de <span className='bg-secondary'>Sebastián Macharette</span>. Gracias por conocer más
+              sobre mí, mis <span className='bg-secondary'>estudios y proyectos</span>."
             </p>
-            <div className="mt-4 flex w-full justify-end sm:mb-40">
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2 }}
+              className="mt-4 flex w-full justify-end sm:mb-40"
+            >
               <button onClick={handleClick} className="btn btn-outline text-white">Conóceme!</button>
-            </div>
-          </div>
-        </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/sections/estudios/Oracle.jsx
+++ src/components/sections/estudios/Oracle.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from 'react';
-import oracle1 from '../../../assets/img/oracle/desarollo personal.png';
-import oracle2 from '../../../assets/img/oracle/logica de programacion.png';
-import oracle3 from '../../../assets/img/oracle/logica de programacion1.png';
+import oracle1 from '../../../assets/img/oracle/1.png';
+import oracle2 from '../../../assets/img/oracle/2.png';
+import oracle3 from '../../../assets/img/oracle/3.png';
+import oracle4 from '../../../assets/img/oracle/4.png';
+import oracle5 from '../../../assets/img/oracle/5.png';
+import oracle6 from '../../../assets/img/oracle/6.png';
+import oracle7 from '../../../assets/img/oracle/7.png';
+import oracle8 from '../../../assets/img/oracle/8.png';
+import oracle9 from '../../../assets/img/oracle/9.png';
+import oracle10 from '../../../assets/img/oracle/10.png';
+import oracle11 from '../../../assets/img/oracle/11.png';
+import oracle12 from '../../../assets/img/oracle/12.png';
+import oracle13 from '../../../assets/img/oracle/13.png';
+import oracle14 from '../../../assets/img/oracle/14.png';
+import oracle15 from '../../../assets/img/oracle/15.png';
+import oracle16 from '../../../assets/img/oracle/16.png';
+import oracle17 from '../../../assets/img/oracle/17.png';
+import oracle18 from '../../../assets/img/oracle/18.png';
+import oracle19 from '../../../assets/img/oracle/19.png';
+import oracle20 from '../../../assets/img/oracle/20.png';
+import oracle21 from '../../../assets/img/oracle/21.png';
+import oracle22 from '../../../assets/img/oracle/22.png';
+import oracle23 from '../../../assets/img/oracle/23.png';
+import oracle24 from '../../../assets/img/oracle/24.png';
+import oracle25 from '../../../assets/img/oracle/25.png';
+import oracle26 from '../../../assets/img/oracle/26.png';
+import oracle27 from '../../../assets/img/oracle/27.png';
+import oracle28 from '../../../assets/img/oracle/28.png';
+import oracle29 from '../../../assets/img/oracle/29.png';
+import oracle30 from '../../../assets/img/oracle/30.png';
+
 
 
 
 function Oracle() {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const images = [oracle1, oracle2, oracle3]; // Imágenes importadas
-
+  const images = [
+    oracle1, oracle2, oracle3, oracle4, oracle5, oracle6, oracle7, oracle8, oracle9, oracle10,
+    oracle11, oracle12, oracle13, oracle14, oracle15, oracle16, oracle17, oracle18, oracle19, oracle20,
+    oracle21, oracle22, oracle23, oracle24, oracle25, oracle26, oracle27, oracle28, oracle29, oracle30
+  ];
+  
   const openModal = (image) => {
     setSelectedImage(image);
     document.getElementById('image_modal').showModal();
